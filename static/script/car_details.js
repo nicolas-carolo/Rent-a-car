18e7d5a0b37c17208c9ec9c5b78fff9dda41f31a
nicolas-carolo <nicolas.carolo@outlook.com>
2019-07-18 23:12:35 +0200

    javascript that makes appear the hire div added

---
+++ static/script/car_details.js
@@ -0,0 +1,6 @@
+function show_div_hire(){
+    document.getElementById("div-hire").style.display = "block";
+    document.getElementById("div-details").style.marginBottom = "0px";
+    document.getElementById("div-hire").style.marginTop = "0px";
+    window.scrollBy(0,150);
+}
