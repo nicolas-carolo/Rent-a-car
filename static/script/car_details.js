60861cc6a351b278c6af84f67c8868d9b833ab7b
nicolas-carolo <nicolas.carolo@outlook.com>
2019-07-19 10:47:56 +0200

    interface for checking car availability

--- static/script/car_details.js
+++ static/script/car_details.js
@@ -1,6 +1,4 @@
 function show_div_hire(){
     document.getElementById("div-hire").style.display = "block";
-    document.getElementById("div-details").style.marginBottom = "0px";
-    document.getElementById("div-hire").style.marginTop = "0px";
     window.scrollBy(0,150);
 }
