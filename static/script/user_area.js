0ac860077fb2f7ee9bb1a85bd92a5ec8e5cf9d8c
nicolas-carolo <nicolas.carolo@outlook.com>
2019-08-14 16:12:14 +0200

    improvements in logic in user area

--- static/script/user_area.js
+++ static/script/user_area.js
@@ -24,3 +24,26 @@ function disable_div_reservations() {
         nodes[i].disabled = true;
     }
 }
+
+function assign_color_to_reservation_status() {
+    var status = document.getElementsByName("reservation-status");
+    for (var i = 0; i < status.length; i++) {
+        if (status[i].innerText == 'Completed') {
+            status[i].style.color = 'gray';
+        } else if (status[i].innerText == 'In progress') {
+            status[i].style.color = 'darkorange';
+        } else {
+            status[i].style.color = 'green';
+        }
+    }
+}
+
+function assign_color_to_delete_reservation_buttons() {
+    var buttons = document.getElementsByName("delete-reservation-button");
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].disabled) {
+            buttons[i].style.background = "gray";
+            buttons[i].style.borderColor = "gray";
+        }
+    }
+}
