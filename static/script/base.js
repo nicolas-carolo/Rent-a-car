e13ab74cb01d290ae560fe4fc7ac37a3e5e82289
Nicolas Carolo <nicolas.carolo@outlook.com>
2019-08-31 16:52:06 +0200

    go to top button added

---
+++ static/script/base.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("go-to-top").style.display = "block";
+    } else {
+        document.getElementById("go-to-top").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
